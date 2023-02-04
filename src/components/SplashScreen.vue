327eb4e6a2288f0be5e2eed55cf7a7e8b583c77a
Sergio Medina <sergiomsystem@gmail.com>
2023-02-04 00:10:39 -0500

    Add splash screen before rendering Home page

---
+++ src/components/SplashScreen.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="splashscreen">
+    <img src="@/assets/cashflow-icon.svg" alt="Logo" />
+    <h1><span>Splash</span>Screen</h1>
+  </div>
+</template>
+
+<style scoped>
+.splashscreen {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  height: 100vh;
+}
+h1 {
+  color: var(--brand-blue);
+}
+h1 span {
+  color: var(--brand-green);
+}
+</style>

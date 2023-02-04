a43c543cd279a8af6413c430f21fe099ea7c3466
Sergio Medina <sergiomsystem@gmail.com>
2023-02-04 00:46:06 -0500

    Create header and layout components

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,3 +1,17 @@
 <template>
-  <div>Hola Financiero</div>
+  <Layout>
+    <template #header><Header></Header></template>
+  </Layout>
 </template>
+
+<script>
+import Layout from "./Layout.vue";
+import Header from "./Header.vue";
+
+export default {
+  components: {
+    Layout,
+    Header,
+  },
+};
+</script>
